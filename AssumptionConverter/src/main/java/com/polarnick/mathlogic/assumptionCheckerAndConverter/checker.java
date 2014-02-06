ae5c3ce1a5f501e467977bea728981190e2aab10
Polarniy Nickolay <PolarHare@gmail.com>
2014-02-06 12:31:24 +0400

    Bug in axioms fixed.

--- AssumptionConverter/src/main/java/com/polarnick/mathlogic/assumptionCheckerAndConverter/checker.java
+++ AssumptionConverter/src/main/java/com/polarnick/mathlogic/assumptionCheckerAndConverter/checker.java
@@ -24,14 +24,14 @@ public class Checker {
     private static final String COMMENT_PREFIX = "//";
 
     private static final String[] unparsedAxioms = new String[]{
-            "C->(B->A)",
+            "A->(B->A)",
             "(A->B)->(A->B->C)->(A->C)",
             "A->B->A&B",
             "A&B->A",
             "A&B->B",
             "A->A|B",
             "B->A|B",
-            "(A->C)->(B->C)->(A&B->C)",
+            "(A->C)->(B->C)->(A|B->C)",
             "(A->B)->(A->!B)->!A",
             "!!A->A"
     };
